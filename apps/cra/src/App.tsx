224618a4879cf2d8654b3a1db6798de81aa6e1c4
Roland Groza <roland.groza@epfl.ch>
2018-04-18 10:54:35 +0200

    feat: illustrate bug with importing ajv

--- apps/cra/src/App.tsx
+++ apps/cra/src/App.tsx
@@ -1,22 +1,26 @@
+import Ajv from 'ajv';
 import * as React from 'react';
-import './App.css';
-import {fooBar} from 'package-b';
 
-const logo = require('./logo.svg');
+const ajv = new Ajv();
+const schema = {
+  type: 'object',
+  properties: {}
+};
 
 class App extends React.Component {
+  validateSchema = () => {
+    const data = {};
+    const valid = ajv.validate(schema, data);
+    if (valid) {
+      // tslint:disable: no-console
+      console.log('Heey :)');
+    }
+  }
+
   render() {
-    const foo = fooBar();
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        {foo}
+      <div>
+        <h1>Welcome to React</h1>
       </div>
     );
   }
